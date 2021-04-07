7bc24a12b6eac262b15ce4b7692ea3600e609488
Pallav14730 <81040573+Pallav14730@users.noreply.github.com>
2021-04-07 20:39:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+
+var items=[];
+var workItems=[];
+
+var day="";
+
+app.get("/", function (request, response) {
+  var today = new Date();
+
+  var option ={
+    weekday:"long",
+    day:"numeric",  
+    month:"long"
+     
+  };
+  var day=today.toLocaleDateString("en-US",option);
+
+response.render("list", { listTitle:day , newListitems:items});
+
+ 
+});
+app.post("/",function(request,response){
+
+    let item =request.body.newItem;
+    if(request.body.list==="work")
+    {
+      workItems.push(item);
+      response.redirect("/work");
+    }
+    else {
+    items.push(item);
+  response.redirect("/");
+    } 
+})
+
+app.get("/Work",function(request,response){
+  response.render("list",{listTitle:"Work List", newListitems:workItems })
+})
+// app.post("/work",function(request,response){
+  
+//   let item =request.body.newItem;
+//   workItems.push(item);
+//   response.redirect("/work")
+// })
+app.listen(3000, function () {
+  console.log("server is running in 3000 port");
+});
